5a30196d37d65161746fd9ee656793bf21e39684
Parry Kwong <parrykwong9@hotmail.com>
2017-05-19 14:09:07 +0800

    Update CVInfo.swift

--- Kwong_Parry_ITE530_A2/CVInfo.swift
+++ Kwong_Parry_ITE530_A2/CVInfo.swift
@@ -1,25 +1,19 @@
 import Foundation
 
-public class CVInfo{ //CV Information
+public class CVInfo{ //CV Information (the info has been hardcoded for this project)
     
-    let name = "Parry Kwong"
-    let age = 23
-    let school = "Charles Sturt University"
-    let major = "Masters of Mobile App Dev."
-    let company = ["linkedin", "facebook", "twitter", "morgan stanley", "google"]
-    let text = ["LinkedIn Internship", "Facebook Internship", "Twitter Programmer", "Morgan Stanley Analyst", "Google Marketing Assistant"]
-    let year = ["2009-2011", "2011-2012","2012-2013","2013-2014","2014-2017"]
-    let jobDescription = [ "-Worked primarily with the marketing team to promote LinkedIn newest features \n-Awarded outstanding staff of the month for June 2009",
-                           
-                           "-Involved in a marketing campaign for new Facebook ad features \n-Pitched to our closest clients regarding FB ad services",
-                           "-Worked on Twitter's backend regarding database support and streamlining data \n-Played vital role in Twitter website updates",
-                           "-Worked with clients for refinancing company assets and liquidation \n-Top performing analyst for year 2014",
-                           "-Pitched and planned for Google's newest marketing campaign on Google Cloud \n-Featured on Google magazine for outstanding performance"
+    let name = //your name
+    let age = //your age
+    let school = //school
+    let major = //major
+    let company = //in array form
+    let text = //in array form
+    let year = //in array form
+    let jobDescription = //in array form
     ]
     
     var imageArray = [String]()
-    var backgroundArray = ["linkedin_background", "facebook_background", "twitter_background",
-                           "morganstanley_background", "google_background"]
+    var backgroundArray = 
     
     func printInfo() -> String{
         
